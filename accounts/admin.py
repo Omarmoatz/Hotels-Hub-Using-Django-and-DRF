66880e7474d6ad63a0b1d8dd2ca96471fd5fea86
Omarmoatz <omar.raouf.002@gmail.com>
2024-05-05 18:26:31 +0300

    add user,profile to admin page & add a signal in the previous commit

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User,Profile
 
-# Register your models here.
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'gender', 'verified', 'created_at']
+    list_filter = ['country', 'verified', 'created_at']
+
+admin.site.register(User)
+admin.site.register(Profile, ProfileAdmin)
