c87f31de698a7473d2c39ce38d97d8413a89091c
Omarmoatz <omar.raouf.002@gmail.com>
2024-05-06 04:41:55 +0300

    add simple sign up  view with template

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,11 +4,11 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import User
 
 class UserForm(UserCreationForm):
-    full_name = forms.CharField(widget=forms.TextInput(attrs={'plaseholder':'Enter Your Full Name'}))
-    username = forms.CharField(widget=forms.TextInput(attrs={'plaseholder':'Enter Your username'}))
-    email = forms.CharField(widget=forms.TextInput(attrs={'plaseholder':'Enter Your email'}))
-    password1 = forms.CharField(widget=forms.TextInput(attrs={'plaseholder':'Enter Your password'}))
-    password2 = forms.CharField(widget=forms.TextInput(attrs={'plaseholder':'Confirm The password2'}))
+    full_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Enter Your Full Name'}))
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Enter Your username'}))
+    email = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Enter Your email'}))
+    password1 = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Enter Your password'}))
+    password2 = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Confirm The password2'}))
     class Meta:
         model = User
         fields = [
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
 from .models import User,Profile
+from .forms import UserForm
 
 def sign_up(request):
-    pass
+    form = UserForm(request.POST)
+
+    return render(request, 'accounts/regester.html', {'form':form})
