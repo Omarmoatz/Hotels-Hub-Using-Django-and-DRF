6ba649d90ae7ee86f5bae35df28d82f01d64528b
Omarmoatz <omar.raouf.002@gmail.com>
2024-05-07 03:28:40 +0300

    add style to sign up page

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,11 +4,22 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import User
 
 class UserForm(UserCreationForm):
-    full_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Enter Your Full Name'}))
-    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Enter Your username'}))
-    email = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Enter Your email'}))
-    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder':'Enter Your password'}))
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder':'Confirm The password'}))
+    full_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Enter Your Full Name',
+                                                              'class':'form-control-lg w-100 border border-1'}))
+    
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Enter Your username',
+                                                             'class':'form-control-lg w-100 border border-1 '}))
+    
+    email = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Enter Your email',
+                                                          'class':'form-control-lg w-100 border border-1'}))
+    
+
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder':'Enter Your password',
+                                                                  'class':'form-control-lg w-100 border border-1'}))
+    
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder':'Confirm The password',
+                                                                  'class':'form-control-lg w-100 border border-1'}))
+    
     class Meta:
         model = User
         fields = [
