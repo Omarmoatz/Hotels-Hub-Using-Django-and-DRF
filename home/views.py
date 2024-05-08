55ac359a4e9995900467e72845c3a62be25fdf10
Omarmoatz <omar.raouf.002@gmail.com>
2024-05-08 18:48:49 +0300

    add home view

--- home/views.py
+++ home/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import MainSettings
+
+def home_view(request):
+    return render(request, 'home.html', {})
--- project/settings.py
+++ project/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
     # my apps
     'accounts',
+    'home',
 
     # 3rd party Apps
 
