3d1174bfafb3e8f5fbf55c3c8902c5ffd4c61894
Omarmoatz <omar.raouf.002@gmail.com>
2024-05-12 21:14:00 +0300

    add model analysis

--- hotel/models.py
+++ hotel/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+
+class Hotel(models.Model):
+    name = ''
+    img = ''
+    description = ''
+    phone = ''
+    address = ''
+    email = ''
+    rating = ''
+    slug = ''
+    created_at = ''
+    tag = ''' sale - new - hot - top - featured - special - popular - 
+            best - offer - discount - cheap - luxury - deal - budget - unique 
+            - trend - exclusive - amazing - great - super'''
+
+    
+
+class HotelGallery(models.Model):
+    Hotel = ''
+    img = ''
+
+
+class HotelFeatures(models.Model):
+    Hotel = ''
+    icon = ''
+    feature = ''
+
+class RoomType(models.Model):
+    hotel = ''
+    title = ''
+    price = ''
+    img = ''
+    beds_num = ''
+    room_size = ''
+    slug = ''
+    created_at = ''
+
+
+
+class Review(models.Model):
+    hotel = ''
+    user = ''
+    review = ''
+    rate = ''
+    created_at = ''
