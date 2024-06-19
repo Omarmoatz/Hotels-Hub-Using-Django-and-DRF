d3cd24a3ac997bb3347054512e6167985b6f5400
Omarmoatz <omar.raouf.002@gmail.com>
2024-06-19 22:47:03 +0300

    finish deigning hotel,gallery and features models

--- hotel/models.py
+++ hotel/models.py
@@ -1,32 +1,57 @@
 from django.db import models
+from django.utils import timezone
+from django.utils.text import slugify
 
+from accounts.models import User
+
+TAG_CHOICES = (
+    ("sale","sale"),
+    ("new","new"),
+    ("featured","featured"),
+    ("cheap","cheap"),
+    ("luxury","luxury"),
+    ("popular","popular")
+)
+
+ICON_FEATURES = (
+    ("bed","bed"),
+    ("bath","bath"),
+    ("wifi","wifi"),
+    ("tv","tv"),
+    ("fan","fan"),
+    ("user-cog","Laundry"),
+    ("utensils","Dinner"),
+)
 
 class Hotel(models.Model):
-    user = ''
-    name = ''
-    img = ''
-    description = ''
-    phone = ''
-    address = ''
-    email = ''
-    rating = ''
-    tag = ''' sale - new - hot - top - featured - special - popular - 
-        best - offer - discount - cheap - luxury - deal - budget - unique 
-        - trend - exclusive - amazing - great - super'''
-    created_at = ''
-    slug = ''
+    user = models.ForeignKey(User, related_name='user_hotel', on_delete=models.CASCADE)
+    name = models.CharField(max_length=500, default='defaul_name', blank=True, null=True)
+    img = models.ImageField( upload_to='hotel/')
+    description = models.TextField(max_length=5000, blank=True, null=True)
+    phone = models.CharField(max_length=500, default='defaul_name', blank=True, null=True)
+    address = models.CharField(max_length=500, default='defaul_name', blank=True, null=True)
+    email = models.EmailField(max_length=500, default='defaul_name', blank=True, null=True)
+    rating = models.PositiveIntegerField()
+    tag = models.CharField( max_length=500, default='defaul_name', choices=TAG_CHOICES, blank=True, null=True)
+    created_at = models.DateTimeField( default=timezone.now)
+    slug = models.SlugField( blank=True, null=True)
 
+    def __str__(self):
+        return self.name
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Hotel, self).save(*args, **kwargs) 
     
 
 class HotelGallery(models.Model):
-    Hotel = ''
-    img = ''
-
+    Hotel = models.ForeignKey( Hotel, related_name='hotel_gallery', on_delete=models.CASCADE)
+    img = models.ImageField( upload_to='hotel gallery/' )
 
 class HotelFeatures(models.Model):
-    Hotel = ''
-    icon = ''
-    feature = ''
+    Hotel = models.ForeignKey( Hotel, related_name='hotel_gallery', on_delete=models.CASCADE)
+    icon = models.CharField( max_length=100, choices=ICON_FEATURES, blank=True, null=True)
+    feature = models.CharField( max_length=500, default='defaul_name', blank=True, null=True)
 
 class RoomType(models.Model):
     hotel = ''
@@ -78,7 +103,7 @@ class Booking(models.Model):
     check_out = ''
     
     created_at = ''
-    
+
 
 
 
