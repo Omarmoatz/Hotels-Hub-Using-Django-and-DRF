0100c2f350373003937c448baeb20496076480f0
Omarmoatz <omar.raouf.002@gmail.com>
2024-06-22 18:09:40 +0300

    add booking, review models

--- hotel/models.py
+++ hotel/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.utils.text import slugify
+from django.utils.crypto import get_random_string
 
 from accounts.models import User
 
@@ -22,7 +23,19 @@ ICON_FEATURES = (
     ("user-cog","Laundry"),
     ("utensils","Dinner"),
 )
-
+PAYMENT_METHOD = (
+    ("cach","cach"),
+    ("visa","visa"),
+    ("strip","strip"),
+    ("paypal","paypal")
+)
+RATE = (
+    (1,1),
+    (2,2),
+    (3,3),
+    (4,4),
+    (5,5)
+)
 class Hotel(models.Model):
     user = models.ForeignKey(User, related_name='user_hotel', on_delete=models.CASCADE)
     name = models.CharField(max_length=500, default='defaul_name', blank=True, null=True)
@@ -102,37 +115,37 @@ class Room(models.Model):
         super(Hotel, self).save(*args, **kwargs) 
 
 class Booking(models.Model):
-    user = ''
-    payment_status = ''
+    user = models.ForeignKey(User, related_name='booked_user', on_delete=models.CASCADE)
+    payment_status = models.CharField( max_length=50, choices=PAYMENT_METHOD)
 
-    full_name = ''
-    phone = ''
-    email = ''
+    full_name = models.CharField( max_length=500, blank=True, null=True)
+    phone = models.CharField( max_length=500, blank=True, null=True)
+    email = models.EmailField( max_length=900, blank=True, null=True)
 
-    hotel = ''
-    room_type = ''
-    room = ''
-    before_discount = ''
-    total = ''
-    money_saved = ''
+    hotel = models.ForeignKey( Hotel, related_name='hotel_booked', on_delete=models.CASCADE)
+    room_type = models.ForeignKey( RoomType, related_name='booked_room_type', on_delete=models.CASCADE)
+    room =  models.ForeignKey( Room, related_name='booked_room', on_delete=models.CASCADE)
+    before_discount = models.DecimalField( max_digits=9, decimal_places=2, blank=True, null=True)
+    total = models.DecimalField( max_digits=9, decimal_places=2, blank=True, null=True)
+    money_saved = models.DecimalField( max_digits=7, decimal_places=2, blank=True, null=True)
     
-    check_in_date = ''
-    check_out_date = ''
-    total_days = ''
-    num_adults = ''
-    num_children = ''
+    check_in_date = models.DateField( default=timezone.now, blank=True, null=True)
+    check_out_date = models.DateField( default=timezone.now, blank=True, null=True)
+    total_days = models.PositiveIntegerField(blank=True, null=True)
+    num_adults = models.PositiveIntegerField(blank=True, null=True)
+    num_children = models.PositiveIntegerField(blank=True, null=True)
 
-    check_in = ''
-    check_out = ''
+    check_in = models.BooleanField(default=False)
+    check_out = models.BooleanField(default=False)
     
-    created_at = ''
-    booking_code = ''
+    created_at = models.DateTimeField( default=timezone.now, blank=True, null=True)
+    booking_code = models.CharField( max_length=500, default=get_random_string(10), blank=True, null=True)
 
 
 
 class Review(models.Model):
-    hotel = ''
-    user = ''
-    review = ''
-    rate = ''
-    created_at = ''
+    hotel = models.ForeignKey( Hotel, related_name='hotel_review', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name='user_review', on_delete=models.CASCADE)
+    content = models.TextField( max_length=1000, blank=True, null=True)
+    rate = models.CharField( max_length=50, choices=RATE)
+    created_at = models.DateTimeField(default=timezone.now, blank=True, null=True)
