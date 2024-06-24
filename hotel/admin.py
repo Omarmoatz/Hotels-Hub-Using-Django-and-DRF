00f0c3ea1079bfbff8646250a6ae3e313743e8b9
Omarmoatz <omar.raouf.002@gmail.com>
2024-06-24 21:25:31 +0300

    add inline items to hotel admin section

--- hotel/admin.py
+++ hotel/admin.py
@@ -2,10 +2,24 @@ from django.contrib import admin
 
 from . import models
 
-admin.site.register(models.Hotel)
-admin.site.register(models.HotelGallery)
-admin.site.register(models.HotelFeatures)
-admin.site.register(models.Room)
-admin.site.register(models.RoomType)
+class HotelGalleryTabular(admin.TabularInline):
+    model = models.HotelGallery
+
+class HotelFeaturesInline(admin.TabularInline):
+    model = models.HotelFeatures
+
+class RoomTypeInline(admin.TabularInline):
+    model = models.RoomType
+    
+class RoomsInline(admin.TabularInline):
+    model = models.Room
+class HotelAdmin(admin.ModelAdmin):
+    inlines = [HotelGalleryTabular, HotelFeaturesInline, RoomTypeInline, RoomsInline]
+    list_display = ['name', 'min_price', 'address', 'user']
+    list_filter = ['name', 'min_price']
+    search_fields = ['name', 'min_price']
+
+
+admin.site.register(models.Hotel, HotelAdmin)
 admin.site.register(models.Booking)
 admin.site.register(models.Review)
