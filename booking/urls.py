771fb4edab41848b27f114c1e1271ec4c9cb1f5a
Omarmoatz <omar.raouf.002@gmail.com>
2024-07-17 15:42:38 +0300

    add room selection view

--- booking/urls.py
+++ booking/urls.py
@@ -7,4 +7,5 @@ app_name = 'booking'
   #   hotels/
 urlpatterns = [
     path('<slug:slug>/check_avilabilty/', views.check_avilability, name='check_avilabilty' ),
+    path('room_selection/', views.room_selection_view, name='room_selection' ),
 ]
--- booking/views.py
+++ booking/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from django.urls import reverse
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, JsonResponse
 from .models import Booking,Hotel,Room,RoomType
 
 
@@ -21,6 +21,51 @@ def check_avilability(request,slug):
         url = reverse('hotel:room_type_detail', args=(slug, room_type.slug))
         url_with_params = f'{url}?id={hotel.id}&name={name}&email={email}&checkin={checkin}&checkout={checkout}&adults={adults}&children={children}&room_type={room_type}'
         return HttpResponseRedirect(url_with_params)
+
+
+
+def room_selection_view(request):
+    data = request.GET
+    room_selection = {}
+
+    room_selection[str(data['id'])] = {
+        'hotel_id' : data['hotel_id'],
+        'hotel_name' : data['hotel_name'],
+        'room_id' : data['room_id'],
+        'room_type' : data['room_type'],
+        'room_price' : data['room_price'],
+        'room_num' : data['room_num'],
+        'room_view' : data['room_view'],
+        'num_of_beds' : data['num_of_beds'],
+        'checkin' : data['checkin'],
+        'checkout' : data['checkout'],
+        'adults' : data['adults'],
+        'children' : data['children'],
+    }
+
+    room_selection_session = request.session['room_selection_obj']
+    if 'room_selection_obj' in request.session:
+        current_id = data['id']
+        if current_id in room_selection_session:
+            old_data = room_selection_session
+            old_data[current_id]['adults'] = int(room_selection[current_id]['adults'])
+            old_data[current_id]['children'] = int(room_selection[current_id]['children'])
+            room_selection_session = old_data
+        else:
+            old_data = room_selection_session
+            old_data.update(room_selection)
+            room_selection_session = old_data
+
+    else:
+        room_selection_session = room_selection
+
+    json_data = {
+        'data': room_selection_session,
+        'name' : 'omar moataz',
+        'data_count' : room_selection_session
+    }
+
+    return JsonResponse(json_data)
 
 
 
