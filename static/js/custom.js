e7e7904143ad82fc4e5b51bb6b4a360f00655b6d
Omarmoatz <omar.raouf.002@gmail.com>
2024-07-18 03:35:56 +0300

    fetch data room data from template by jquery

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,3 +1,35 @@
 $(document).ready(function(){
-    console.log('connected')
+
+    $('.add-to-selection').on('click', function(){
+        let room_id = $(this).attr("data-index");
+        
+        let hotel_id = $('.hotel-id').val()
+        let hotel_name= $('.hotel-name').val()
+        let room_type= $('.room-type').val()
+
+        let room_price= $('.room-price').val() 
+        let room_num = $('.room-num').val() 
+        let room_view= $('.room-view').val()
+        let num_of_bed= $('.beds-num').val()
+
+        let checkin = $('.checkin').val()
+        let checkout = $('.checkout').val() 
+        let adults = $('.adults').val()
+        let children = $('.children').val() 
+
+        console.log(room_id);
+        console.log(hotel_id);
+        console.log(hotel_name);
+        console.log(room_type);
+
+        console.log(room_price);
+        console.log(room_num);
+        console.log(room_view);
+        console.log(num_of_bed);
+
+        console.log(checkin);
+        console.log(checkout);
+        console.log(adults);
+        console.log(children);
+    })
 })
