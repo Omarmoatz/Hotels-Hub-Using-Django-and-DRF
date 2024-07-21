de2a8a4fe6583ac3db3229508ca47c6b0e19dd8c
Omarmoatz <omar.raouf.002@gmail.com>
2024-07-21 04:00:54 +0300

    move selected rooms view to booking app

--- booking/urls.py
+++ booking/urls.py
@@ -6,6 +6,8 @@ app_name = 'booking'
 
   #   hotels/
 urlpatterns = [
+    path('selected_rooms/', views.selected_rooms, name='selected_rooms' ),
     path('<slug:slug>/check_avilabilty/', views.check_avilability, name='check_avilabilty' ),
     path('room_selection/', views.room_selection_view, name='room_selection' ),
+    path('create_booking/', views.create_booking, name='create_booking' ),
 ]
