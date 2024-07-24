666c9bce53428b29579c9d175d3bc777a5825efc
Omarmoatz <omar.raouf.002@gmail.com>
2024-07-24 15:43:58 +0300

    design coupun model

--- booking/models.py
+++ booking/models.py
@@ -42,3 +42,14 @@ class Booking(models.Model):
 
     def __str__(self):
         return f'{str(self.user)}----{self.hotel}----{self.full_name}'
+    
+
+    class Coupon(models.Model):
+        code = models.CharField( max_length=100, blank=True, null=True)
+        discount = models.PositiveIntegerField( blank=True, null=True)
+        quantity = models.PositiveIntegerField( blank=True, null=True)
+        start_date = models.DateTimeField(default=timezone.now)
+        end_date = models.DateTimeField( blank=True, null=True)
+
+        def __str__(self):
+            return self.code
