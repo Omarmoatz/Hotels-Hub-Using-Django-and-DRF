8c15799c3c05ce289f90d2d02a25a66a17314864
omarMoatz <omar.raouf.002@gmail.com>
2024-09-09 11:08:21 +0300

    update hotel models

--- hotel/models.py
+++ hotel/models.py
@@ -2,48 +2,32 @@ from django.db import models
 from django.utils import timezone
 from django.utils.text import slugify
 from django.db.models.aggregates import  Avg
+from django.utils.translation import gettext_lazy as _
 
 from accounts.models import User
 
-TAG_CHOICES = (
-    ("sale","sale"),
-    ("new","new"),
-    ("featured","featured"),
-    ("cheap","cheap"),
-    ("luxury","luxury"),
-    ("popular","popular")
-)
-
-ICON_FEATURES = (
-    ("bed","bed"),
-    ("bath","bath"),
-    ("wifi","wifi"),
-    ("tv","tv"),
-    ("fan","fan"),
-    ("user-cog","Laundry"),
-    ("utensils","Dinner"),
-)
-RATE = (
-    ('1',"1"),
-    ('2','2'),
-    ('3',"3"),
-    ('4','4'),
-    ('5','5')
-)
 class Hotel(models.Model):
+    class TAG_CHOICES(models.TextChoices):
+        SALE = 'sale', _('Sale')
+        NEW = 'new', _('New')
+        FEATURED = 'featured', _('Featured')
+        CHEAP = 'cheap', _('Cheap')
+        LUXURY = 'luxury', _('Luxury')
+        POPULAR = 'popular', _('Popular')
+
     user = models.ForeignKey(User, related_name='user_hotel', on_delete=models.CASCADE)
-    name = models.CharField(max_length=200, default='defaul_name', blank=True, null=True)
+    name = models.CharField(max_length=200, default='', blank=True, null=True)
     img = models.ImageField( upload_to='hotel/')
     subtitle = models.TextField(max_length=400, blank=True, null=True)
     description = models.TextField(max_length=5000, blank=True, null=True)
     min_price = models.DecimalField( max_digits=7, decimal_places=2, blank=True, null=True)
     max_price = models.DecimalField( max_digits=7, decimal_places=2, blank=True, null=True)
-    phone = models.CharField(max_length=500, default='defaul_phone', blank=True, null=True)
-    address = models.CharField(max_length=500, default='defaul_address', blank=True, null=True)
-    email = models.EmailField(max_length=500, default='defaul_email', blank=True, null=True)
+    phone = models.CharField(max_length=500, default='', blank=True, null=True)
+    address = models.CharField(max_length=500, default='', blank=True, null=True)
+    email = models.EmailField(max_length=500, default='', blank=True, null=True)
     feature = models.ManyToManyField( 'HotelFeatures')
-    tag = models.CharField( max_length=500, default='defaul_name', choices=TAG_CHOICES, blank=True, null=True)
-    created_at = models.DateTimeField( default=timezone.now)
+    tag = models.CharField( max_length=500, default='', choices=TAG_CHOICES.choices, blank=True, null=True)
+    created_at = models.DateTimeField( auto_now=True)
     slug = models.SlugField( blank=True, null=True)
 
     def avg_rating(self):
@@ -69,20 +53,29 @@ class HotelGallery(models.Model):
         return str(self.hotel)
     
 class HotelFeatures(models.Model):
-    icon = models.CharField( max_length=100, choices=ICON_FEATURES, blank=True, null=True)
-    feature = models.CharField( max_length=500, default='defaul_name', blank=True, null=True)
+    class ICON_FEATURES(models.TextChoices):
+        BED = 'bed', _('Bed')
+        BATH = 'bath', _('Bath')
+        WIFI = 'wifi', _('WiFi')
+        TV = 'tv', _('TV')
+        FAN = 'fan', _('Fan')
+        LAUNDRY = 'user-cog', _('Laundry')
+        DINNER = 'utensils', _('Dinner')
+
+    icon = models.CharField( max_length=100, choices=ICON_FEATURES.choices, blank=True, null=True)
+    feature = models.CharField( max_length=500, default='', blank=True, null=True)
 
     def __str__(self):
         return self.feature
     
 class RoomType(models.Model):
     hotel = models.ForeignKey( Hotel, related_name='hotel_room_type', on_delete=models.CASCADE)
-    title = models.CharField( max_length=500, default='defaul_name', blank=True, null=True)
+    title = models.CharField( max_length=500, default='', blank=True, null=True)
     price_start = models.DecimalField( max_digits=7, decimal_places=2)
     price_end = models.DecimalField( max_digits=7, decimal_places=2)
     beds_num = models.PositiveIntegerField()    
     room_size = models.PositiveIntegerField()
-    created_at = models.DateTimeField( default=timezone.now)
+    created_at = models.DateTimeField( auto_now=True)
     slug = models.SlugField( blank=True, null=True)
 
     def __str__(self):
@@ -98,10 +91,10 @@ class Room(models.Model):
     hotel = models.ForeignKey( Hotel, related_name='hotel_room', on_delete=models.CASCADE)
     room_type = models.ForeignKey( RoomType, related_name='room_type', on_delete=models.CASCADE)
     room_num = models.PositiveIntegerField()
-    view = models.CharField( max_length=500, default='defaul_view', blank=True, null=True)
+    view = models.CharField( max_length=500, default='', blank=True, null=True)
     price = models.DecimalField( max_digits=7, decimal_places=2, blank=True, null=True)
     is_available = models.BooleanField(default=True)
-    created_at = models.DateTimeField( default=timezone.now)
+    created_at = models.DateTimeField( auto_now=True)
     slug = models.SlugField( blank=True, null=True)
 
     def beds_num(self):
@@ -120,11 +113,18 @@ class Room(models.Model):
 
 
 class Review(models.Model):
+    class RATE(models.IntegerChoices):
+        ONE = 1
+        TWO = 2
+        THREE = 3
+        FOUR = 4
+        FIVE = 5
+
     hotel = models.ForeignKey( Hotel, related_name='hotel_review', on_delete=models.CASCADE)
     user = models.ForeignKey(User, related_name='user_review', on_delete=models.CASCADE)
     content = models.TextField( max_length=1000, blank=True, null=True)
-    rate = models.CharField( max_length=50, choices=RATE)
-    created_at = models.DateTimeField(default=timezone.now, blank=True, null=True)
+    rate = models.CharField( max_length=50, choices=RATE.choices, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f'{str(self.user)}----{self.hotel}'
+        return f'Review {self.id} by {str(self.user)} on {self.hotel}'
--- project/settings.py
+++ project/settings.py
@@ -155,7 +155,7 @@ JAZZMIN_SETTINGS = {
     "site_brand": "Admin",
 
     # Logo to use for your site, must be present in static files, used for brand on top left
-    "site_logo": "../media/company/hotel.png",
+    "site_logo": "../media/company/HOTEL.png",
 
     # Logo to use for your site, must be present in static files, used for login form logo (defaults to site_logo)
     "login_logo": None,
