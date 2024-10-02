7a3f6b9de960019cf4a649051da2b7ae8430a214
omarMoatz <omar.raouf.002@gmail.com>
2024-10-02 10:42:57 +0300

    add project migrations

--- config/settings/local.py
+++ config/settings/local.py
@@ -41,19 +41,19 @@ INSTALLED_APPS = ["whitenoise.runserver_nostatic", *INSTALLED_APPS]
 # django-debug-toolbar
 # ------------------------------------------------------------------------------
 # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#prerequisites
-INSTALLED_APPS += ["debug_toolbar"]
+# INSTALLED_APPS += ["debug_toolbar"]
 # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#middleware
 # MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]
 # https://django-debug-toolbar.readthedocs.io/en/latest/configuration.html#debug-toolbar-config
-DEBUG_TOOLBAR_CONFIG = {
-    "DISABLE_PANELS": [
-        "debug_toolbar.panels.redirects.RedirectsPanel",
-        # Disable profiling panel due to an issue with Python 3.12:
-        # https://github.com/jazzband/django-debug-toolbar/issues/1875
-        "debug_toolbar.panels.profiling.ProfilingPanel",
-    ],
-    "SHOW_TEMPLATE_CONTEXT": True,
-}
+# DEBUG_TOOLBAR_CONFIG = {
+#     "DISABLE_PANELS": [
+#         "debug_toolbar.panels.redirects.RedirectsPanel",
+#         # Disable profiling panel due to an issue with Python 3.12:
+#         # https://github.com/jazzband/django-debug-toolbar/issues/1875
+#         "debug_toolbar.panels.profiling.ProfilingPanel",
+#     ],
+#     "SHOW_TEMPLATE_CONTEXT": True,
+# }
 # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#internal-ips
 INTERNAL_IPS = ["127.0.0.1", "10.0.2.2"]
 if env("USE_DOCKER") == "yes":
